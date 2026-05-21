a27985e7d91deb08b923aa075134eaaf15c56c92
gagaggg1 <grimmagrim@gmail.com>
2026-05-21 21:56:50 +0000

    hwrwer

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ if input_mode == "Ползунки":
     st.sidebar.text(f"📉 Расчетный D30: {d30_calc:.1f}%")
     st.sidebar.markdown("---")
 
-    base_conv = st.sidebar.slider("Базовая конверсия в донат (%):", 0.5, 10.0, value=2.5, step=0.1) / 100.0
-    base_arppu = st.sidebar.slider("Базовый чек донатера (R$):", 50, 2000, value=280, step=10)
+    base_conv = st.sidebar.slider("Конверсия доната (%):", 0.5, 10.0, value=2.5, step=0.1) / 100.0
+    base_arppu = st.sidebar.slider("Средний чек (R$):", 50, 2000, value=280, step=10)
     
     # БЛОК НАСТРОЕК CREATOR REWARDS (Элитная логика)
     st.sidebar.subheader("💎 Creator Rewards")
